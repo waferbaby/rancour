96763b9550b2dc29f85b0815c5c217c7bc4db203
Daniel Bogan <d@waferbaby.com>
2025-03-02 17:29:02 +1100

    Add the initial client and base module

---
+++ lib/rancor/client.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'httpx'
+
+module Rancor
+  class Client
+    DISCORD_API_VERSION = 10
+
+    def initialize(app_id:, bot_token: nil, bearer_token: nil)
+      raise 'You must supply either a bot or bearer token' if bot_token.nil? && bearer_token.nil?
+
+      @app_id = app_id
+
+      @bot_token = bot_token if bot_token.present?
+      @bearer_token = bearer_token if bearer_token.present?
+    end
+
+    def register_application_commands(payload:, guild_id: nil)
+      path = '/commands'
+      path = "/guilds/#{guild_id}" + path if guild_id.present?
+
+      response = connection.post(path, json: payload)
+      response.raise_for_status
+
+      response.json
+    end
+
+    private
+
+    def connection
+      @connection ||= HTTPX.with(
+        origin: 'https://discord.com',
+        base_path: "/api/v#{DISCORD_API_VERSION}/applications/#{@app_id}",
+        headers: {
+          authorization: authorization_header
+        }
+      )
+    end
+
+    def authorization_header
+      return "Bearer #{@bearer_token}" if @bearer_token.present?
+
+      "Bot #{@bot_token}"
+    end
+  end
+end
