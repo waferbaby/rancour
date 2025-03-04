4b51ba6853b8c132a30e95212ade2cdacfce2c6e
Daniel Bogan <d@waferbaby.com>
2025-03-04 19:26:34 +1100

    Rework the HTTP methods and error handling

--- lib/rancour/client.rb
+++ lib/rancour/client.rb
@@ -15,26 +15,68 @@ module Rancour
       @bearer_token = bearer_token if bearer_token.present?
     end
 
-    def register_application_commands(payload:, guild_id: nil)
-      path = '/commands'
-      path = "/guilds/#{guild_id}" + path if guild_id.present?
+    def fetch_application_commands(guild_id: nil)
+      get(commands_path(guild_id: guild_id))
+    end
 
-      response = connection.post(path, json: payload)
-      response.raise_for_status
+    def register_application_commands(payload, guild_id: nil)
+      post(commands_path(guild_id: guild_id), params: payload)
+    end
 
-      response.json
+    def delete_application_command(command_id, guild_id: nil)
+      delete(commands_path(guild_id: guild_id) + command_id)
     end
 
     private
 
-    def connection
-      @connection ||= HTTPX.with(
+    def commands_path(guild_id: nil)
+      "#{"/guides/#{guild_id}" if guild_id.present?}/commands/"
+    end
+
+    def get(endpoint)
+      process_response(http_client.get(endpoint, headers: { 'content-type': 'application/json' }))
+    end
+
+    def post(endpoint, params: {})
+      process_response(http_client.post(endpoint, json: params))
+    end
+
+    def patch(endpoint, params: {})
+      process_response(http_client.put(endpoint, json: params))
+    end
+
+    def delete(endpoint)
+      puts "I am sending a delete to #{endpoint}"
+      process_response(http_client.delete(endpoint))
+    end
+
+    def http_client
+      @http_client ||= HTTPX.with(
         origin: 'https://discord.com',
         base_path: "/api/v#{DISCORD_API_VERSION}/applications/#{@app_id}",
         headers: {
           authorization: authorization_header
         }
       )
+    end
+
+    def process_response(response)
+      response.raise_for_status
+      response.json
+    rescue HTTPX::TimeoutError, HTTPX::ResolveError => e
+      raise RequestError, e.message
+    rescue HTTPX::HTTPError => e
+      handle_api_error(e)
+    end
+
+    def handle_api_error(error)
+      klass = case error.response.status
+              when 400..499 then RequestError
+              when 500..599 then ResponseError
+              else Error
+              end
+
+      raise klass || Error, error.response.json['message']
     end
 
     def authorization_header
