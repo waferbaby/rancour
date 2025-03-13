422bae3d07e2ec3beb01ab70aeee490e1995c71a
Daniel Bogan <d@waferbaby.com>
2025-03-13 19:22:33 +1100

    Require the new classes

--- lib/rancour.rb
+++ lib/rancour.rb
@@ -2,7 +2,11 @@
 
 $LOAD_PATH.unshift(__dir__)
 
+require 'rancour/response'
+require 'rancour/guild'
+require 'rancour/user'
 require 'rancour/errors'
 require 'rancour/client'
 require 'rancour/interaction'
 require 'rancour/webhook'
+require 'rancour/images'
