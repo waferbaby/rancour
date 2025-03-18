6af2e7d90321d44da81e5983698e192b3974bfb4
Daniel Bogan <d@waferbaby.com>
2025-03-18 19:27:18 +1100

    Fix the module/class

--- lib/rancour/interaction/response.rb
+++ lib/rancour/interaction/response.rb
@@ -1,27 +1,29 @@
 # frozen_string_literal: true
 
 module Rancour
-  class InteractionResponse
-    PONG = 1
-    CHANNEL_MESSAGE_WITH_SOURCE = 4
-    DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE = 5
-    DEFERRED_UPDATE_MESSAGE = 6
-    UPDATE_MESSAGE = 7
-    APPLICATION_COMMAND_AUTOCOMPLETE_RESULT = 8
-    MODAL = 9
-    LAUNCH_ACTIVITY = 12
+  module Interaction
+    class Response
+      PONG = 1
+      CHANNEL_MESSAGE_WITH_SOURCE = 4
+      DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE = 5
+      DEFERRED_UPDATE_MESSAGE = 6
+      UPDATE_MESSAGE = 7
+      APPLICATION_COMMAND_AUTOCOMPLETE_RESULT = 8
+      MODAL = 9
+      LAUNCH_ACTIVITY = 12
 
-    attr_accessor :type, :data
+      attr_accessor :type, :data
 
-    def initialize(type:, data: nil)
-      self.type = type
-      self.data = data
-    end
+      def initialize(type:, data: nil)
+        self.type = type
+        self.data = data
+      end
 
-    def to_h
-      {}.tap do |output|
-        output[:type] = type
-        output[:data] = data.to_h if data.present?
+      def to_h
+        {}.tap do |output|
+          output[:type] = type
+          output[:data] = data.to_h if data.present?
+        end
       end
     end
   end
