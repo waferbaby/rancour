3dd667f70dce42a5fbdb00b1b997b685b86b38de
Daniel Bogan <d@waferbaby.com>
2025-03-20 19:36:27 +1100

    Add the message response class

---
+++ lib/rancour/interaction/message_response.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Rancour
+  module Interaction
+    class MessageResponse < Response
+      extend Forwardable
+
+      def_delegators :@data, :add_embed
+
+      def initialize
+        super(type: CHANNEL_MESSAGE_WITH_SOURCE, data: Message.new)
+      end
+    end
+  end
+end
